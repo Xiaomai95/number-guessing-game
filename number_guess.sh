d04402f2e5ad52c558bc8e8600bca0026fd9d8a4
Xiao Mai <ethanmyers1995@gmail.com>
2024-10-28 18:39:12 +0000

    feat: again

--- number_guess.sh
+++ number_guess.sh
@@ -20,10 +20,10 @@ START() {
     # get following data: games_played, best_game
     GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME'")
     BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME'")
-    echo "Welcome back, $(echo "$SEARCH_USERNAME" | sed -r 's/^ *| *$//g')! You have played $(echo "$GAMES_PLAYED" | sed -r 's/^ *| *$//g') games, and your best game took $(echo "$BEST_GAME" | sed -r 's/^ *| *$//g') guesses."
+    echo "Welcome back, $SEARCH_USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
     # Print: Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses.
     else
-    echo "Welcome, $(echo "$USERNAME" | sed -r 's/^ *| *$//g')! It looks like this is your first time here."
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
     ADD_NAME_TO_DATABASE=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$USERNAME', 0, 999)")
   fi
 
@@ -48,39 +48,26 @@ USER_GUESS() {
   fi
   if [[ $GUESS -gt $RANDOM_NUMBER ]]
     then
+     ((NUMBER_OF_GUESSES++))
     echo "It's lower than that, guess again:"
-    ((NUMBER_OF_GUESSES++))
     USER_GUESS
     elif [[ $GUESS -lt $RANDOM_NUMBER ]]
       then
+       ((NUMBER_OF_GUESSES++))
       echo "It's higher than that, guess again:"
-      ((NUMBER_OF_GUESSES++))
       USER_GUESS
-    elif [[ $GUESS -eq $RANDOM_NUMBER ]]
-      then
-      ((NUMBER_OF_GUESSES++))
+    else
       ((GAMES_PLAYED++))
       UPDATE_GAMES_PLAYED=$($PSQL "UPDATE users SET games_played = $GAMES_PLAYED WHERE username = '$USERNAME'")
+      echo "games played: $GAMES_PLAYED"
 
-      if [[ $NUMBER_OF_GUESSES -lt $BEST_GAME || -z $BEST_GAME  ]]
+      if [[ $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
         then
         UPDATE_BEST_GAME=$($PSQL "UPDATE users SET best_game = $NUMBER_OF_GUESSES WHERE username = '$USERNAME'")
       fi
-      echo "You guessed it in $(echo "$NUMBER_OF_GUESSES" | sed -r 's/^ *| *$//g') tries. The secret number was $(echo "$RANDOM_NUMBER" | sed -r 's/^ *| *$//g'). Nice job!"
+      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $RANDOM_NUMBER. Nice job!"
   fi
   
 }
-
-# UPDATE_DATA() {
-  
-#   ((GAMES_PLAYED++))
-#   UPDATE_GAMES_PLAYED=$($PSQL "UPDATE users SET games_played = $GAMES_PLAYED WHERE username = '$USERNAME'")
-
-#   if [[ $NUMBER_OF_GUESSES -lt $BEST_GAME || -z $BEST_GAME  ]]
-#     then
-#     UPDATE_BEST_GAME=$($PSQL "UPDATE users SET best_game = $NUMBER_OF_GUESSES WHERE username = '$USERNAME'")
-#   fi
-  
-# }
 
 START
